d040818a4327230673f7712b1a1fa865fe26213e
nikishkaa <nikita.goutor@gmail.com>
2024-04-15 00:01:52 +0300

    update.

--- src/by/itstep/goutor/javalesson/lesson40/model/entyti/condiment/CondimentDecorator.java
+++ src/by/itstep/goutor/javalesson/lesson40/model/entyti/condiment/CondimentDecorator.java
@@ -5,6 +5,7 @@ import by.itstep.goutor.javalesson.lesson40.model.entyti.Beverage;
 public abstract class CondimentDecorator extends Beverage {
     protected Beverage beverage;
 
+
     @Override
     public abstract String getDescription();
 }
